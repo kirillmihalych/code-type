50d9d01279d5d3545bd0beee00b792715434ee1e
kirillmihalych <tiltlessman@gmail.com>
2025-01-05 22:43:07 +0300

    basic keymap

---
+++ src/components/KeyLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="transition-colors duration-100 w-10 h-10 font-bold flex justify-center items-center rounded-sm text-gray-600"
+    :class="props.value ? 'bg-purple-400' : 'bg-purple-200'"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["value"]);
+</script>
---
+++ src/components/KeymapLayout.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="grid place-content-center place-items-center">
+    <div class="row-first flex gap-2 mb-2">
+      <KeyLayout :value="q"> q </KeyLayout>
+      <KeyLayout :value="w"> w </KeyLayout>
+      <KeyLayout :value="e"> e </KeyLayout>
+      <KeyLayout :value="r"> r</KeyLayout>
+      <KeyLayout :value="t"> t </KeyLayout>
+      <KeyLayout :value="y"> y </KeyLayout>
+      <KeyLayout :value="u"> u </KeyLayout>
+      <KeyLayout :value="i"> i </KeyLayout>
+      <KeyLayout :value="o"> o </KeyLayout>
+      <KeyLayout :value="p"> p </KeyLayout>
+      <KeyLayout :value="BracketLeft"> [ </KeyLayout>
+      <KeyLayout :value="BracketRight"> ] </KeyLayout>
+    </div>
+    <div class="row-second flex gap-2 mb-2">
+      <KeyLayout :value="a"> a </KeyLayout>
+      <KeyLayout :value="s"> s </KeyLayout>
+      <KeyLayout :value="d"> d </KeyLayout>
+      <KeyLayout :value="f"> f</KeyLayout>
+      <KeyLayout :value="g"> g </KeyLayout>
+      <KeyLayout :value="h"> h </KeyLayout>
+      <KeyLayout :value="j"> j </KeyLayout>
+      <KeyLayout :value="k"> k </KeyLayout>
+      <KeyLayout :value="l"> l </KeyLayout>
+      <KeyLayout :value="Semicolon"> ; </KeyLayout>
+      <KeyLayout :value="Quote"> ' </KeyLayout>
+    </div>
+    <div class="row-second flex gap-2 mb-2">
+      <KeyLayout :value="z"> z </KeyLayout>
+      <KeyLayout :value="x"> x </KeyLayout>
+      <KeyLayout :value="c"> c </KeyLayout>
+      <KeyLayout :value="v"> v</KeyLayout>
+      <KeyLayout :value="b"> b </KeyLayout>
+      <KeyLayout :value="n"> n </KeyLayout>
+      <KeyLayout :value="m"> m </KeyLayout>
+      <KeyLayout :value="Comma"> , </KeyLayout>
+      <KeyLayout :value="Period"> . </KeyLayout>
+      <KeyLayout :value="Slash"> / </KeyLayout>
+    </div>
+    <KeyLayout :value="Space" style="width: 350px"> default </KeyLayout>
+  </div>
+</template>
+
+<script setup>
+import { useMagicKeys } from "@vueuse/core";
+import KeyLayout from "./KeyLayout.vue";
+
+const {
+  q,
+  w,
+  e,
+  r,
+  t,
+  y,
+  u,
+  i,
+  o,
+  p,
+  a,
+  s,
+  d,
+  f,
+  g,
+  h,
+  j,
+  k,
+  l,
+  z,
+  x,
+  c,
+  v,
+  b,
+  n,
+  m,
+  Space,
+  BracketRight,
+  BracketLeft,
+  Quote,
+  Semicolon,
+  Period,
+  Comma,
+  Slash,
+} = useMagicKeys();
+</script>

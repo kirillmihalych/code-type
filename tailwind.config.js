a980c5a53aa8d3d8c50a7e52c93bcb0864b98e73
kirillmihalych <tiltlessman@gmail.com>
2025-01-18 16:50:50 +0300

    install tailwind scroll bar plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindScrollbar from "tailwind-scrollbar";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
@@ -37,5 +39,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindScrollbar],
 };

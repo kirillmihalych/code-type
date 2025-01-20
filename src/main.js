7e1f69c363058da036cb53131a5ebfb4708a74f5
kirillmihalych <tiltlessman@gmail.com>
2025-01-20 22:08:56 +0300

    <refactor>: remove dependencies

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { router } from "./router";
 import { VueQueryPlugin } from "@tanstack/vue-query";
-import PrimeVue from "primevue/config";
 import App from "./App.vue";
 
 const pinia = createPinia();
 const app = createApp(App);
 
-app.use(PrimeVue);
 app.use(pinia);
 app.use(router);
 app.use(VueQueryPlugin);

779e26082cbac5dd54acd96f0f305db79294ff02
kirillmihalych <tiltlessman@gmail.com>
2025-01-22 18:16:51 +0300

    <feat>: custom dragndrop animation

--- src/components/CustomThemeList.vue
+++ src/components/CustomThemeList.vue
@@ -1,40 +1,48 @@
 <template>
   <div class="text-sub">
     <div>
+      <!-- @start="drag = true"
+      @end="drag = false" -->
       <h2>Фавориты</h2>
       <draggable
         group="custom-themes"
         :list="colorThemeStore.favoriteCustomThemes"
-        @start="drag = true"
-        @end="drag = false"
+        ghost-class="moving-card"
         item-key="id"
+        :animation="75"
         class="flex flex-col md:flex-row gap-2 w-full min-h-16 border-2 border-sub rounded-md p-2"
       >
         <template #item="{ element }">
-          <CustomThemeCard
-            :name="element.name"
-            :properties="element.properties"
-            :id="element.id"
-          />
+          <div class="w-full">
+            <CustomThemeCard
+              :name="element.name"
+              :properties="element.properties"
+              :id="element.id"
+            />
+          </div>
         </template>
       </draggable>
     </div>
     <div>
+      <!-- @start="drag = true"
+      @end="drag = false" -->
       <h2>Созданные темы</h2>
       <draggable
-        :list="colorThemeStore.savedThemes"
         group="custom-themes"
-        @start="drag = true"
-        @end="drag = false"
+        :list="colorThemeStore.savedThemes"
+        ghost-class="moving-card"
+        :animation="75"
         item-key="id"
         class="flex flex-col md:flex-row gap-2 w-full min-h-16 border-2 border-sub rounded-md p-2"
       >
         <template #item="{ element }">
-          <CustomThemeCard
-            :name="element.name"
-            :properties="element.properties"
-            :id="element.id"
-          />
+          <div class="w-full">
+            <CustomThemeCard
+              :name="element.name"
+              :properties="element.properties"
+              :id="element.id"
+            />
+          </div>
         </template>
       </draggable>
     </div>
@@ -49,7 +57,8 @@ import draggable from "vuedraggable";
 const colorThemeStore = useColorThemeStore();
 </script>
 
-<style>
-/* 
- */
+<style scoped>
+.moving-card {
+  @apply w-full opacity-15 rounded-md border-2 border-black;
+}
 </style>

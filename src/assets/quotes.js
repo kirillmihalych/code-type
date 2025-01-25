aa9a62b5abbd5e9ddd0fce310d1331ad41b49e0f
kirillmihalych <tiltlessman@gmail.com>
2025-01-25 16:49:33 +0300

    <chore>: add quotes from true detectivee

--- src/assets/quotes.js
+++ src/assets/quotes.js
@@ -19,4 +19,24 @@ export const quotes = [
     id: 5,
     text: "All that is gold does not glitter, Not all those who wander are lost; The old that is strong does not wither, Deep roots are not reached by the frost. From the ashes a fire shall be woken, A light from the shadows shall spring; Renewed shall be blade that was broken, The crownless again shall be king.",
   },
+  {
+    id: 6,
+    text: "If the only thing keeping a person decent is the expectation of divine reward then, brother, that person is a piece of shit. And I’d like to get as many of them out in the open as possible. You gotta get together and tell yourself stories that violate every law of the universe just to get through the goddamn day? What’s that say about your reality?",
+  },
+  {
+    id: 7,
+    text: "The world needs bad men. We keep the other bad men from the door.",
+  },
+  {
+    id: 8,
+    text: "To realize that all your life—you know, all your love, all your hate, all your memory, all your pain—it was all the same thing. It was all the same dream. A dream that you had inside a locked room. A dream about being a person. And like a lot of dreams there's a monster at the end of it.",
+  },
+  {
+    id: 9,
+    text: "My life's been a circle of violence and degradation long as I can remember. I'm ready to tie it off.",
+  },
+  {
+    id: 10,
+    text: "Well, once there was only dark. If you ask me, the light's winning.",
+  },
 ];

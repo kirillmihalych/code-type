2ec9286382b12b6a3103aecf059c5ecdd8d08e29
kirillmihalych <tiltlessman@gmail.com>
2025-02-26 13:42:40 +0300

    <fix>: trimEnd input

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -613,8 +613,7 @@ watch(currentInput, (newInputValue, oldInputValue) => {
     newInput.value = newInputValue;
     let charsLength = newInputValue.length - oldInputValue.length;
     if (charsLength > 1) {
-      currentInput.value = newInputValue.trim();
-      charsLength -= 1;
+      currentInput.value = newInputValue.trimEnd();
     }
     handleAddExtra();
     if (appearanceStore.isTapeMode) {
